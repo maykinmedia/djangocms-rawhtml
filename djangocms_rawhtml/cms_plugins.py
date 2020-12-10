532a04a44cba2405e77ec380a7b87221a4df5ebe
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2020-12-10 14:20:37 +0100

    Allowed full with and added some extra html plugins

--- djangocms_rawhtml/cms_plugins.py
+++ djangocms_rawhtml/cms_plugins.py
@@ -1,7 +1,10 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext as _
-from djangocms_rawhtml.models import RawHTMLPluginData
+from djangocms_rawhtml.models import (
+    RawHTMLPluginData, TwoColumnRawHTMLPluginData, ThreeColumnRawHTMLPluginData,
+    FourColumnRawHTMLPluginData
+)
 
 
 @plugin_pool.register_plugin
@@ -14,7 +17,55 @@ class RawHTMLPluginPublisher(CMSPluginBase):
     change_form_template = "admin/djangocms_rawhtml/change_form.html"
     render_template = "djangocms_rawhtml/plugins/raw.html"
     allow_children = False
-    
+
+    def render(self, context, instance, placeholder):
+        context.update({ 'instance': instance })
+        return context
+
+
+@plugin_pool.register_plugin
+class TwoColumnRawHTMLPluginPublisher(CMSPluginBase):
+    """
+    Plugin to manage ran HTML text that needs to be managed from the admin interface.
+    """
+    name = _("2 Column Raw HTML")
+    model = TwoColumnRawHTMLPluginData
+    change_form_template = "admin/djangocms_rawhtml/change_form.html"
+    render_template = "djangocms_rawhtml/plugins/two_column_raw.html"
+    allow_children = False
+
+    def render(self, context, instance, placeholder):
+        context.update({ 'instance': instance })
+        return context
+
+
+@plugin_pool.register_plugin
+class ThreeColumnRawHTMLPluginPublisher(CMSPluginBase):
+    """
+    Plugin to manage ran HTML text that needs to be managed from the admin interface.
+    """
+    name = _("3 Column Raw HTML")
+    model = ThreeColumnRawHTMLPluginData
+    change_form_template = "admin/djangocms_rawhtml/change_form.html"
+    render_template = "djangocms_rawhtml/plugins/three_column_raw.html"
+    allow_children = False
+
+    def render(self, context, instance, placeholder):
+        context.update({ 'instance': instance })
+        return context
+
+
+@plugin_pool.register_plugin
+class FourColumnRawHTMLPluginPublisher(CMSPluginBase):
+    """
+    Plugin to manage ran HTML text that needs to be managed from the admin interface.
+    """
+    name = _("4 Column Raw HTML")
+    model = FourColumnRawHTMLPluginData
+    change_form_template = "admin/djangocms_rawhtml/change_form.html"
+    render_template = "djangocms_rawhtml/plugins/four_column_raw.html"
+    allow_children = False
+
     def render(self, context, instance, placeholder):
         context.update({ 'instance': instance })
         return context
--- djangocms_rawhtml/models.py
+++ djangocms_rawhtml/models.py
@@ -9,15 +9,83 @@ from django.utils.html import strip_tags
 class RawHTMLPluginData(CMSPlugin):
     """
     Model for Raw HTML Plugin
-    
+
     body - contains the HTML source text
     label - used for display purposes on the admin site
     """
 
     label = models.CharField(max_length=40, default="")
     body = models.TextField('HTML')
+    full_width = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+    def __unicode__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+
+@python_2_unicode_compatible
+class TwoColumnRawHTMLPluginData(CMSPlugin):
+    """
+    Model for Raw HTML Plugin
+
+    body - contains the HTML source text
+    label - used for display purposes on the admin site
+    """
+
+    label = models.CharField(max_length=40, default="")
+    body_left = models.TextField('HTML left')
+    body_right = models.TextField('HTML right')
+    full_width = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+    def __unicode__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+
+@python_2_unicode_compatible
+class ThreeColumnRawHTMLPluginData(CMSPlugin):
+    """
+    Model for Raw HTML Plugin
+
+    body - contains the HTML source text
+    label - used for display purposes on the admin site
+    """
+
+    label = models.CharField(max_length=40, default="")
+    body_left = models.TextField('HTML left')
+    body_middle = models.TextField('HTML middle')
+    body_right = models.TextField('HTML right')
+    full_width = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+    def __unicode__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
+
+@python_2_unicode_compatible
+class FourColumnRawHTMLPluginData(CMSPlugin):
+    """
+    Model for Raw HTML Plugin
+
+    body - contains the HTML source text
+    label - used for display purposes on the admin site
+    """
+
+    label = models.CharField(max_length=40, default="")
+    body_left = models.TextField('HTML left')
+    body_middle_left = models.TextField('HTML middle left')
+    body_middle_right = models.TextField('HTML middle right')
+    body_right = models.TextField('HTML right')
+    full_width = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
+
     def __unicode__(self):
         return Truncator(strip_tags(self.label).replace('&shy;', '')).words(5, truncate="...")
